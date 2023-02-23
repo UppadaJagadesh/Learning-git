147dd4199b3768858e616bd6b8a4f7e324f0d5c5
juppada <juppada@miraclesoft.com>
2023-02-24 00:03:26 +0530

    Changed to posted the claims

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/claims', (req, res) => {
 })
 
 app.post('/claims', (req,res)=>{
-    console.log("Posted the claim");
+    console.log("Posted the claims");
 })
 app.listen('3000', () => {
     console.log('server is running');
